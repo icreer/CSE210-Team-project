13a198b4c978968b1a1ac7c7fd04c7a0e415b758
Yat Lam <lam20034@byui.edu>
2022-02-08 09:43:48 -0700

    jumper

--- unit03-jumper/Classes/Jumper.cs
+++ unit03-jumper/Classes/Jumper.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
 
 namespace unit03_jumper
 {
     class Jumper
     {
+        private bool cutParachute = false;
+
+        private List<string> parachuteList = new List<string>();
+
+        // add the parts of parachute into the parachute list.
+        public void CreateParachutes()
+        {
+            parachuteList.Add("  ___");
+            parachuteList.Add(" /___\\");
+            parachuteList.Add(" \\   /");
+            parachuteList.Add("  \\ /");
+            parachuteList.Add("   o");
+            parachuteList.Add("  /|\\");
+            parachuteList.Add("  / \\");
+        }
+        
+        public void DoUpdate(bool guessRight)
+        {
+            if (guessRight)
+            {
+                cutParachute = false;
+            }
+            else
+            {
+                cutParachute = true;
+            }
+        }
+
+        public void CutParachuts()
+        {
+            if (cutParachute)
+            {
+                parachuteList.RemoveAt(0);
+            }
+        }
+
+        public void GetDisplayParachuts()
+        {
+            foreach(string part in parachuteList)
+            {
+                Console.WriteLine(part);
+            }
+            Console.WriteLine("");
+        }
 
     }
 
