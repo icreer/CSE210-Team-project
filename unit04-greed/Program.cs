53cd94b3428b00fc3ae17c93dca175ca02db1038
Isaac <isaaccreer@gmail.com>
2022-02-25 13:12:10 -0700

    Fixing Bugs

--- unit04-greed/Program.cs
+++ unit04-greed/Program.cs
@@ -14,7 +14,7 @@ namespace Unit04
     /// </summary>
     class Program
     {
-        private static int FRAME_RATE = 12;
+        private static int FRAME_RATE = 30;
         private static int MAX_X = 900;
         private static int MAX_Y = 600;
         private static int CELL_SIZE = 15;
@@ -52,34 +52,26 @@ namespace Unit04
             banner.SetPosition(new Point(CELL_SIZE, 0));
             cast.AddActor("banner", banner);
 
-        //     // create the robot
-        //     Actor robot = new Actor();
-        //     robot.SetText("#");
-        //     robot.SetFontSize(FONT_SIZE);
-        //     robot.SetColor(WHITE);
-        //     robot.SetPosition(new Point(MAX_X / 2, MAX_Y / 2));
-        //     cast.AddActor("robot", robot);
-            //bool creat = true;
             int count = 0;
             Random rand = new Random();
             //while(creat)
             for(count = 0; count <= 20; count++)
             {
                 
-                //if(count == 100)
-               // {
-                   // creat = false;
-               // }
                     Actor gem = new Actor();
                     gem.SetText("*");
                     gem.SetFontSize(FONT_SIZE);
-                    gem.SetColor(WHITE);
+                    int r = random.Next(0, 256);
+                    int g = random.Next(0, 256);
+                    int b = random.Next(0, 256);
+                    Color color = new Color(r, g, b);
+                    gem.SetColor(color);
                     int x = rand.Next(1, 5);
                     int y = 0;
-                    int dx = rand.Next(1, 5);
+                    int dx = 0;
                     int dy = rand.Next(1, 5);
                     Point position = new Point(x, y);
-                    gem.SetPosition(position);
+                    gem.SetPosition(new Point (random.Next(0,900),MAX_Y));
                     Point velocity = new Point(dx,dy);
                     gem.SetVelocity(velocity);
                     //count++;
@@ -101,85 +93,16 @@ namespace Unit04
                     rock.SetColor(color_rock);
                     int rx = rand.Next(1, 5);
                     int ry = 0;
-                    int rdx = random.Next(1, 5);
+                    int rdx = 0;
                     int rdy = random.Next(1, 5);
                     Point rposition = new Point(rx, ry);
                     rock.SetPosition(new Point(random.Next(0,900),MAX_Y));
-                    //Point rvelocity = new Point(rdx,rdy);
-                    //rock.SetVelocity(rvelocity);
+                    Point rvelocity = new Point(rdx,rdy);
+                    rock.SetVelocity(rvelocity);
                    // add++;
                     cast.AddActor("rocks", rock);
                 }
                     
-            // load the messages
-           // List<string> messages = File.ReadAllLines(DATA_PATH).ToList<string>();
-
-        //     // load the messages
-        //    List<string> messages = File.ReadAllLines(DATA_PATH).ToList<string>();
-
-            Actor robot = new Actor();
-        /*     robot.SetText("#");
-             robot.SetFontSize(FONT_SIZE);
-             robot.SetColor(WHITE);
-             robot.SetPosition(new Point(MAX_X / 2, MAX_Y / 2));
-            cast.AddActor("robot", robot);
-        */
-             int error = 1;
-
-            Actor gems = new Actor();
-            gems.SetText("*");
-            gems.SetFontSize(FONT_SIZE);
-            int r = random.Next(0, 256);
-            int g = random.Next(0, 256);
-            int b = random.Next(0, 256);
-            Color color = new Color(r, g, b);
-            int place = MAX_X/random.Next(1,10);
-            gems.SetColor(color);
-            gems.SetPosition(new Point(place , MAX_Y ));
-            cast.AddActor("gems", gems);
-
-            Actor rocks = new Actor();
-            rocks.SetText("o");
-            rocks.SetFontSize(FONT_SIZE);
-            //int rr = random.Next(0, 256);
-            //int gr = random.Next(0, 256);
-            //int br = random.Next(0, 256);
-            //Color color_rock = new Color(rr, gr, br);
-            //rocks.SetColor(color_rock);
-            int place_rock = MAX_X/random.Next(1,10);
-            rocks.SetPosition(new Point( place_rock,MAX_Y ));
-            cast.AddActor("rocks", rocks);
-
-
-            // load the messages
-           // List<string> messages = File.ReadAllLines(DATA_PATH).ToList<string>();
-
-        //     // create the artifacts
-            
-        //     for (int i = 0; i < DEFAULT_ARTIFACTS; i++)
-        //      {
-        //          string text = ((char)random.Next(33, 126)).ToString();
-        //          string message = messages[i];
-
-        //          int x = random.Next(1, COLS);
-        //          int y = random.Next(1, ROWS);
-        //          Point position = new Point(x, y);
-        //          position = position.Scale(CELL_SIZE);
-
-        //          int r = random.Next(0, 256);
-        //          int g = random.Next(0, 256);
-        //          int b = random.Next(0, 256);
-        //          Color color = new Color(r, g, b);
-
-        //          Artifact artifact = new Artifact();
-        //          artifact.SetText(text);
-        //          artifact.SetFontSize(FONT_SIZE);
-        //          artifact.SetColor(color);
-        //          artifact.SetPosition(position);
-        //          artifact.SetMessage(message);
-        //          cast.AddActor("artifacts", artifact);
-        //      }
-
             // start the game
             KeyboardService keyboardService = new KeyboardService(CELL_SIZE);
             VideoService videoService 
