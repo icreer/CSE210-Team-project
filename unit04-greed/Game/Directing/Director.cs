fb2e737e2ea73423d0d1d5dfde26690a7a447ae6
Isaac <isaaccreer@gmail.com>
2022-02-26 07:43:37 -0700

    Clean up

--- unit04-greed/Game/Directing/Director.cs
+++ unit04-greed/Game/Directing/Director.cs
@@ -19,8 +19,6 @@ namespace Unit04.Game.Directing
         Cast cast = new Cast();
         Random random = new Random();
 
-        //Gems gem = new Gems();
-        //Gems rock = new Gems();
         private static int MAX_X = 900;
         private static int MAX_Y = 600;
         private static int FONT_SIZE = 30;
@@ -49,8 +47,6 @@ namespace Unit04.Game.Directing
                 GetInputs(cast);
                 DoUpdates(cast);
                 DoOutputs(cast);
-                // set_create_gems();
-                // set_create_rocks();
             }
             videoService.CloseWindow();
         }
@@ -193,47 +189,7 @@ namespace Unit04.Game.Directing
             videoService.DrawActors(actors);
             videoService.FlushBuffer();
         }
-        // public void set_create_gems()
-        // {
-        //     Actor gem = new Actor();
-        //     gem.SetText("*");
-        //     gem.SetFontSize(FONT_SIZE);
-        //     int r = random.Next(0, 256);
-        //     int g = random.Next(0, 256);
-        //     int b = random.Next(0, 256);
-        //     Color color = new Color(r, g, b);
-        //     gem.SetColor(color);
-        //     int x = random.Next(1, 5);
-        //     int y = 0;
-        //     int dx = 0;
-        //     int dy = random.Next(1, 5);
-        //     Point position = new Point(x, y);
-        //     gem.SetPosition(new Point (random.Next(0,900),MAX_Y));
-        //     Point velocity = new Point(dx,dy);
-        //     gem.SetVelocity(velocity);
-        //     cast.AddActor("gems", gem);
-        // }
-
-        // public void set_create_rocks()
-        // {
-        //     Actor rock = new Actor();
-        //     rock.SetText("o");
-        //     rock.SetFontSize(FONT_SIZE);
-        //     int rr = random.Next(0, 256);
-        //     int gr = random.Next(0, 256);
-        //     int br = random.Next(0, 256);
-        //     Color color_rock = new Color(rr, gr, br);
-        //     rock.SetColor(color_rock);
-        //     int rx = random.Next(1, 5);
-        //     int ry = 0;
-        //     int rdx = 0;
-        //     int rdy = random.Next(1, 5);
-        //     Point rposition = new Point(rx, ry);
-        //     rock.SetPosition(new Point(random.Next(0,900),MAX_Y));
-        //     Point rvelocity = new Point(rdx,rdy);
-        //     rock.SetVelocity(rvelocity);
-        //     cast.AddActor("rocks", rock);
-        // }
+       
         public bool IsCollionsion(Actor One,Actor Two)
         {
             int size = CELL_SIZE;
