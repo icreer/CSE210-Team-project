910647ad72a4fe1cb539d50dc7c557ddda388d75
Isaac <isaaccreer@gmail.com>
2022-03-03 10:14:58 -0700

    Snake game upload

--- unit05-cycle/Program.cs
+++ unit05-cycle/Program.cs
@@ -6,7 +6,26 @@ namespace unit05_cycle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create the cast
+            Cast cast = new Cast();
+            cast.AddActor("food", new Food());
+            cast.AddActor("snake", new Snake());
+            cast.AddActor("score", new Score());
+
+            // create the services
+            KeyboardService keyboardService = new KeyboardService();
+            VideoService videoService = new VideoService(false);
+           
+            // create the script
+            Script script = new Script();
+            script.AddAction("input", new ControlActorsAction(keyboardService));
+            script.AddAction("update", new MoveActorsAction());
+            script.AddAction("update", new HandleCollisionsAction());
+            script.AddAction("output", new DrawActorsAction(videoService));
+
+            // start the game
+            Director director = new Director(videoService);
+            director.StartGame(cast, script);
         }
     }
 }
