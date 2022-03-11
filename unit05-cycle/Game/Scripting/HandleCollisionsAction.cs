fe93c3667d4b6a1031b415b8b0880f9fdf452d8e
Yat Lam <lam20034@byui.edu>
2022-03-11 14:06:39 -0700

    handle collision

--- unit05-cycle/Game/Scripting/HandleCollisionsAction.cs
+++ unit05-cycle/Game/Scripting/HandleCollisionsAction.cs
@@ -42,84 +42,144 @@ namespace unit05_cycle.Scripting
         /// <param name="cast">The cast of actors.</param>
         private void HandleSegmentCollisions(Cast cast)
         {
+            // Actor message = new Actor();
+
+            // int x = Constants.MAX_X / 2;
+            // int y = Constants.MAX_Y / 2;
+            // Point position = new Point(x, y);
+
+            // List<Actor> actors = cast.GetActors("cycle");
+            // foreach (Actor actor in actors)
+            // {
+            //     Cycle snake = (Cycle)actor;
+            //     Actor head = snake.GetHead();
+            //     List<Actor> body = snake.GetBody();
+
+            //     foreach (Actor segment in body)
+            //     {
+            //         if (segment.GetPosition().Equals(head.GetPosition()))
+            //         {
+            //             isGameOver = true;
+            //             actor.SetColor(Constants.WHITE);
+            //             message.SetText("Player Two Wins!!");
+            //             message.SetPosition(position);
+            //             cast.AddActor("messages", message);
+            //         }
+            //     }
+            // }
+            // List<Actor> actors1 = cast.GetActors("cycle2");
+            // foreach (Actor actor in actors1)
+            // {
+            //     Cycle snake = (Cycle)actor;
+            //     Actor head = snake.GetHead();
+            //     List<Actor> body = snake.GetBody();
+
+            //     foreach (Actor segment in body)
+            //     {
+            //         if (segment.GetPosition().Equals(head.GetPosition()))
+            //         {
+            //             isGameOver = true;
+            //             actor.SetColor(Constants.WHITE);
+            //             message.SetText("Player One Wins!!");
+            //             message.SetPosition(position);
+            //             cast.AddActor("messages", message);
+            //         }
+            //     }
+            // }
+            // foreach (Actor actor in actors)
+            // {
+            //     Cycle snake = (Cycle)actor;
+            //     Actor head = snake.GetHead();
+            //     List<Actor> body = snake.GetBody();
+            //     foreach (Actor segment in body)
+            //     {
+            //         foreach (Actor actor1 in actors1)
+            //         {
+            //             Cycle snake1 = (Cycle)actor1;
+            //             Actor head1 = snake1.GetHead();
+            //             List<Actor> body1 = snake1.GetBody();
+            //             foreach (Actor segment1 in body1)
+            //             {
+            //                 if (segment.GetPosition().Equals(head1.GetPosition())) 
+            //                 {
+            //                     isGameOver = true;
+            //                     actor.SetColor(Constants.WHITE);
+            //                     actor1.SetColor(Constants.WHITE);
+            //                     message.SetText("Player One Wins!!");
+            //                     message.SetPosition(position);
+            //                     cast.AddActor("messages", message);
+            //                 }
+            //                 else if (segment1.GetPosition().Equals(head.GetPosition()))
+            //                 {
+            //                     isGameOver = true;
+            //                     actor.SetColor(Constants.WHITE);
+            //                     actor1.SetColor(Constants.WHITE);
+            //                     message.SetText("Player Two Wins!!");
+            //                     message.SetPosition(position);
+            //                     cast.AddActor("messages", message);
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
+
             Actor message = new Actor();
 
             int x = Constants.MAX_X / 2;
             int y = Constants.MAX_Y / 2;
             Point position = new Point(x, y);
 
-            List<Actor> actors = cast.GetActors("cycle");
-            foreach (Actor actor in actors)
-            {
-                Cycle snake = (Cycle)actor;
-                Actor head = snake.GetHead();
-                List<Actor> body = snake.GetBody();
+            Actor actor1 = cast.GetFirstActor("cycle");
+            Cycle snake1 = (Cycle)actor1;
+            Actor head1 = snake1.GetHead();
+            List<Actor> body1 = snake1.GetBody();
 
-                foreach (Actor segment in body)
+            Actor actor2 = cast.GetFirstActor("cycle1");
+            Cycle snake2 = (Cycle)actor2;
+            Actor head2 = snake2.GetHead();
+            List<Actor> body2 = snake2.GetBody();
+
+            foreach (Actor segment in body1)
+            {
+                if (segment.GetPosition().Equals(head1.GetPosition()))
                 {
-                    if (segment.GetPosition().Equals(head.GetPosition()))
-                    {
-                        isGameOver = true;
-                        actor.SetColor(Constants.WHITE);
-                        message.SetText("Player Two Wins!!");
-                        message.SetPosition(position);
-                        cast.AddActor("messages", message);
-                    }
+                    isGameOver = true;
+                    actor1.SetColor(Constants.WHITE);
+                    actor2.SetColor(Constants.WHITE);
+                    message.SetText("Player Two Wins!!");
+                    message.SetPosition(position);
+                    cast.AddActor("messages", message);
+                }
+                else if (segment.GetPosition().Equals(head2.GetPosition()))
+                {
+                    isGameOver = true;
+                    actor1.SetColor(Constants.WHITE);
+                    actor2.SetColor(Constants.WHITE);
+                    message.SetText("Player One Wins!!");
+                    message.SetPosition(position);
+                    cast.AddActor("messages", message);
                 }
             }
-            List<Actor> actors1 = cast.GetActors("cycle2");
-            foreach (Actor actor in actors1)
-            {
-                Cycle snake = (Cycle)actor;
-                Actor head = snake.GetHead();
-                List<Actor> body = snake.GetBody();
 
-                foreach (Actor segment in body)
-                {
-                    if (segment.GetPosition().Equals(head.GetPosition()))
-                    {
-                        isGameOver = true;
-                        actor.SetColor(Constants.WHITE);
-                        message.SetText("Player One Wins!!");
-                        message.SetPosition(position);
-                        cast.AddActor("messages", message);
-                    }
-                }
-            }
-            foreach (Actor actor in actors)
+            foreach (Actor segment in body2)
             {
-                Cycle snake = (Cycle)actor;
-                Actor head = snake.GetHead();
-                List<Actor> body = snake.GetBody();
-                foreach (Actor segment in body)
+                if (segment.GetPosition().Equals(head1.GetPosition()))
                 {
-                    foreach (Actor actor1 in actors1)
-                    {
-                        Cycle snake1 = (Cycle)actor1;
-                        Actor head1 = snake1.GetHead();
-                        List<Actor> body1 = snake1.GetBody();
-                        foreach (Actor segment1 in body1)
-                        {
-                            if (segment.GetPosition().Equals(head1.GetPosition())) 
-                            {
-                                isGameOver = true;
-                                actor.SetColor(Constants.WHITE);
-                                actor1.SetColor(Constants.WHITE);
-                                message.SetText("Player One Wins!!");
-                                message.SetPosition(position);
-                                cast.AddActor("messages", message);
-                            }
-                            else if (segment1.GetPosition().Equals(head.GetPosition()))
-                            {
-                                isGameOver = true;
-                                actor.SetColor(Constants.WHITE);
-                                actor1.SetColor(Constants.WHITE);
-                                message.SetText("Player Two Wins!!");
-                                message.SetPosition(position);
-                                cast.AddActor("messages", message);
-                            }
-                        }
-                    }
+                    isGameOver = true;
+                    actor1.SetColor(Constants.WHITE);
+                    actor2.SetColor(Constants.WHITE);
+                    message.SetText("Player Two Wins!!");
+                    message.SetPosition(position);
+                    cast.AddActor("messages", message);
+                }
+                else if (segment.GetPosition().Equals(head2.GetPosition()))
+                {
+                    isGameOver = true;
+                    actor1.SetColor(Constants.WHITE);
+                    actor2.SetColor(Constants.WHITE);
+                    message.SetText("Player One Wins!!");
+                    message.SetPosition(position);
+                    cast.AddActor("messages", message);
                 }
             }
         }
